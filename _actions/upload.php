fae60af8e3878b6d14215d8f2c9b30e3781c9c50
Thet Pai Soe(Keesh) <thetpai0904@gmail.com>
2024-09-07 22:09:53 +0630

    Update upload.php

--- _actions/upload.php
+++ _actions/upload.php
@@ -4,7 +4,7 @@ $type = $_FILES['photo']['type'];
 $tmp_name = $_FILES['photo']['tmp_name'];
 
 if ($type == "image/jpeg" or $type == "image/png") {
-    move_uploaded_file($tmp_name, "photos/profile.jpeg");
+    move_uploaded_file($tmp_name, "photos/profiles.jpeg");
     header("location: ../profile.php");
 } else {
     header("location: ../profile.php?error=type");
